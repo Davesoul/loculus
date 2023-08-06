bf07889b7d98e42eb395f6d067319a8955db1c18
Davesoul <davekinimo77@gmail.com>
2023-08-06 01:40:28 +0000

    improved structure based on model, view and controller

--- README.md
+++
@@ -1,7 +0,0 @@
-# Loculus
-
-'Loculus' in latin designate a small place where we can store items. This concept represents the thought behind this project.
-
-features:
-- account creation
-- files storing
--- windowTemplate.php
+++
@@ -1,7 +0,0 @@
-<div class="objbar up">
-    <h6>The Art of Electronics ( PDFDrive )</h6>
-    <span><i class="fa-solid fa-close"></i></span>
-</div>
-<iframe src="./index.php" frameborder="0"></iframe>
-<br>
-
